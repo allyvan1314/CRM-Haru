1bd87cdb83d1b1a187fa2f403e919fe4c46c28eb
hoangvan1410 <76679911+hoangvan1410@users.noreply.github.com>
2021-08-09 12:06:39 +0700

    Update sendCampaign.js

--- redirect/sendCampaign/sendCampaign.js
+++ redirect/sendCampaign/sendCampaign.js
@@ -3,10 +3,10 @@ const express = require("express");
 const router = express.Router();
 const apiAdapter = require("./apiAdapter");
 
-const BASE_URL = "http://otp.voip24h.vn/v2/create";
+const BASE_URL = "http://otp.voip24h.vn/v2/";
 const api = apiAdapter(BASE_URL);
 
-router.post("/", (req, res) => {
+router.post("/create", (req, res) => {
     api.post(req.originalUrl, req.body).then(resp => {
         res.send(resp.data)
     })
