a75b7ee02c78834203d34bbe461fb000bac75058
hoangvan1410 <49230626+allyvan1314@users.noreply.github.com>
2021-08-11 19:31:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 
 // Setup server port
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 5000;
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
