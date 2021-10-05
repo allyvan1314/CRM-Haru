cee577b2f88b1eef937549f4d9b76ed62195ad6a
Ngọc Ánh Nguyễn <49230626+allyvan1314@users.noreply.github.com>
2021-10-05 11:03:37 +0700

    update uat

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 
 // Setup server port
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
