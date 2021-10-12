d7aed92fbbd246ee15d8e50d6a41f2b5e7517171
Ngọc Ánh Nguyễn <49230626+allyvan1314@users.noreply.github.com>
2021-10-12 14:20:01 +0700

    edit vmg leadStatus

--- controllers/leadStatus.controller.js
+++ controllers/leadStatus.controller.js
@@ -2,35 +2,52 @@ const leadStatus = require('../models/leadStatus.model.js');
 const leadStatusRepository = require('../repository/leadStatus.repository');
 const response = require("../Config/responsive/handle");
 const sendLog = require("../models/sendLog.model.js")
-const sendLogRepository = require("../repository/sendLog.repository.js")
+const sendLogRepository = require("../repository/sendLog.repository.js");
+const { crossOriginResourcePolicy } = require('helmet');
 
 module.exports.create = async (req, res) => {
-    let {phone,vmg_receive_date,vmg_code,fico_lead_import_date,fico_lead_status,fico_lead_message,fico_app_date,
-        fico_app_status,fico_decision_date,fico_loan_status,fico_disbursed_LA,fico_disbursed_date} = req.body;
+    let {
+        status,
+        reason,
+        requestid
+    } = req.body;
 
-    let info = new leadStatus({
-        phone,vmg_receive_date,vmg_code,fico_lead_import_date,fico_lead_status,fico_lead_message,fico_app_date,
-        fico_app_status,fico_decision_date,fico_loan_status,fico_disbursed_LA,fico_disbursed_date
-    });
-    let id = info._id.toHexString();
+    
 
-    if(phone == null ){
-        res.send(response.handleNullPhoneError(null, "Phone Number null"));
+    let json = req.body.reason;
+    function IsJsonString(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+    if (IsJsonString(json)) {
+        const obj = JSON.parse(json);
+        reason = obj;
+        let info = new leadStatus({
+            status,
+            reason,
+            requestid
+        });
+        await leadStatusRepository.addLeadStatus(info)
+        res.sendStatus(200);
         return;
     }
+    else{
+        let info = new leadStatus({
+            status,
+            reason,
+            requestid
+        });
+        await leadStatusRepository.addLeadStatus(info)
+        res.sendStatus(200);
+    }
+    
 
-    let data = sendLogRepository.getLogByPhone(phone)
-    data.then( async function(result) {
-        //console.log(result) // "Some User token"
-        if(result == ""){
-            res.send(response.handleInvalidPhoneError(null, "Phone Number invalid"));
-            return;
-        }
-        else{
-            await leadStatusRepository.addLeadStatus(info)
-            res.send(response.handleSuccess({id,phone},"Success"))
-            return;
-        }
-        })
+
+
+    //await leadStatusRepository.addLeadStatus(info)
 
 }
--- models/leadStatus.model.js
+++ models/leadStatus.model.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose")
 const leadStatusSchema = new mongoose.Schema({
-    phone: String,
-    vmg_receive_date: String,
-    vmg_code: String,
-    fico_lead_import_date: String,
-    fico_lead_status: String,
-    fico_lead_message: String,
-    fico_app_date: String,
-    fico_app_status: String,
-    fico_decision_date: String,
-    fico_loan_status: String,
-    fico_disbursed_LA: String,
-    fico_disbursed_date: String,
+    status: String,
+    reason: JSON||String,
+    requestid: String,
+})
+const reasonDetail = new mongoose.Schema({
+    traceNo:String,
+    code: Number,
+    message: String,
+    Date: String,
+    data:Array
 })
 
 module.exports = mongoose.model("leadStatus",leadStatusSchema)
